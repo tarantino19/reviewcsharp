549bdaa1468f45762b70556ab180bbed8b819c64
Red Tolentino <tolentinored19@gmail.com>
2026-05-23 13:51:11 +0800

    add entity framework basics - still unsecured/for local only

---
+++ GameStore.Api/Data/GameStoreContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using GameStore.Api.Entities;
+
+namespace GameStore.Api.Data;
+
+public class GameStoreContext (DbContextOptions<GameStoreContext> options) : DbContext(options)
+{
+    public DbSet<Game> Games => Set<Game>();
+    public DbSet<Genre> Genres => Set<Genre>();
+}
+
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,7 +1,13 @@
 using GameStore.Api.Endpoints;
+using GameStore.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+var connString = "Data Source=GameStore.db";
+
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 
 app.MapGamesEndpoints();
 
